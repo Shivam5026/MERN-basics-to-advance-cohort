b3918bb3b321bba659ba3740299614088c87d138
shivam <shivamkumar5026@gmail.com>
2024-01-01 23:24:26 +0530

    working on assignments

---
+++ Assignments/week-3/03-mongo/routes/user.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
+
+// User Routes
+router.post('/signup', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username,
+        password
+    })
+    res.json({message: 'User created successfully'})
+});
+
+router.get('/courses', (req, res) => {
+    // Implement listing all courses logic
+});
+
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
+    // Implement course purchase logic
+});
+
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
+    // Implement fetching purchased courses logic
+});
+
+module.exports = router
